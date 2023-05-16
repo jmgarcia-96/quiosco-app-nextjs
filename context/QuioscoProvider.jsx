76721c676b0303f69ff366aeca09064d489fa1fb
jesgarlan <96166634+jmgarcia-96@users.noreply.github.com>
2023-05-16 16:05:29 +0200

    Proyecto Quiosco App hasta seccion 28

---
+++ context/QuioscoProvider.jsx
@@ -0,0 +1,112 @@
+import { useState, useEffect, createContext } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
+
+const QuioscoContext = createContext();
+
+const QuioscoProvider = ({ children }) => {
+  const [categorias, setCategorias] = useState([]);
+  const [categoriaActual, setCategoriaActual] = useState({});
+  const [producto, setProducto] = useState({});
+  const [modal, setModal] = useState(false);
+  const [pedido, setPedido] = useState([]);
+  const [nombre, setNombre] = useState("");
+  const [total, setTotal] = useState("");
+
+  const router = useRouter();
+
+  useEffect(() => {
+    obtenerCategorias();
+  }, []);
+
+  useEffect(() => {
+    setCategoriaActual(categorias[0]);
+  }, [categorias]);
+
+  useEffect(() => {
+    const nuevoTotal = pedido.reduce(
+      (total, producto) => producto.precio * producto.cantidad + total,
+      0
+    );
+    setTotal(nuevoTotal);
+  }, [pedido]);
+
+  const obtenerCategorias = async () => {
+    const { data } = await axios("/api/categorias");
+    setCategorias(data);
+  };
+
+  const handleClickCategoria = (id) => {
+    const categoria = categorias.find((cat) => cat.id === id);
+    setCategoriaActual(categoria);
+    router.push("/");
+  };
+
+  const handleSetProducto = (producto) => {
+    setProducto(producto);
+  };
+
+  const handleChangeModal = () => {
+    setModal(!modal);
+  };
+
+  const handleAgregarPedido = ({ categoriaId, ...producto }) => {
+    if (pedido.some((productoState) => productoState.id === producto.id)) {
+      const pedidoActualizado = pedido.map((productoState) =>
+        productoState.id === producto.id ? producto : productoState
+      );
+      setPedido(pedidoActualizado);
+      toast.success("Guardado correctamente");
+    } else {
+      setPedido([...pedido, producto]);
+      toast.success("Agregado al pedido");
+    }
+    setModal(false);
+  };
+
+  const handleEditarCantidades = (id) => {
+    const productoActualizar = pedido.filter((producto) => producto.id === id);
+    setProducto(productoActualizar[0]);
+    setModal(!modal);
+  };
+
+  const handleEliminarProducto = (id) => {
+    const pedidoActualizado = pedido.filter((producto) => producto.id !== id);
+    setPedido(pedidoActualizado);
+  };
+
+  const colocarOrden = async (e) => {
+    e.preventDefault();
+    console.log(pedido);
+    console.log(nombre);
+    console.log(total);
+  };
+  return (
+    <QuioscoContext.Provider
+      value={{
+        categorias,
+        categoriaActual,
+        handleClickCategoria,
+        handleSetProducto,
+        producto,
+        handleChangeModal,
+        modal,
+        handleAgregarPedido,
+        pedido,
+        handleEditarCantidades,
+        handleEliminarProducto,
+        nombre,
+        setNombre,
+        colocarOrden,
+        total,
+      }}
+    >
+      {children}
+    </QuioscoContext.Provider>
+  );
+};
+
+export { QuioscoProvider };
+
+export default QuioscoContext;
---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Layout from "../layout/Layout";
+import useQuiosco from "../hooks/useQuiosco";
+import Producto from "../components/Producto";
+
+export default function Home() {
+  const { categoriaActual } = useQuiosco();
+  return (
+    <Layout pagina={`Menú ${categoriaActual?.nombre}`}>
+      <h1 className="text-4xl font-black">{categoriaActual?.nombre}</h1>
+      <p className="text-2xl my-10">
+        Elige y personaliza tu pedido a continuación
+      </p>
+      <div className="grid gap-4 grid-cols1 md:grid-cols2 xl:grid-cols-3 2xl:grid-cols-4">
+        {categoriaActual?.productos?.map((producto) => (
+          <Producto key={producto.id} producto={producto} />
+        ))}
+      </div>
+    </Layout>
+  );
+}
